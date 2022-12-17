dac21c5d440fa74ec7ad738ee658d11700d84567
hasan <mdhasanhk.98@gmail.com>
2022-12-17 22:04:41 +0600

    value vs refference

--- starter/script.js
+++ starter/script.js
@@ -24,3 +24,30 @@ createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 ////////////////////////////////////////////
 */
+
+////////////////////////////////////////////
+const flight = 'LH123';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 125458754,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr ' + passenger.name;
+  if (passenger.passport === 125458754) {
+    alert('Checked in');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
